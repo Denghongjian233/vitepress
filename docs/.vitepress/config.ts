b82bc99b3cab323a45799a01aab7ff56c604ecd1
dhj <17613071153@163.com>
2023-04-02 19:33:21 +0800

    4

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,9 +1,18 @@
+/*
+ * @Author: dhj 17613071153@163.com
+ * @Date: 2023-04-02 19:19:37
+ * @LastEditors: dhj 17613071153@163.com
+ * @LastEditTime: 2023-04-02 19:33:18
+ * @FilePath: \实验项目\docs\.vitepress\config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "My Awesome Project",
   description: "A VitePress Site",
+  base:'/vitepress/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
