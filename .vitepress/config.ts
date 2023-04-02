b84853d2e85e25e5cdcaad0e49633bca21232b3d
dhj <17613071153@163.com>
2023-04-02 19:23:18 +0800

    feat:项目初始化

---
+++ .vitepress/config.ts
@@ -0,0 +1,37 @@
+/*
+ * @Author: dhj 17613071153@163.com
+ * @Date: 2023-04-02 19:18:41
+ * @LastEditors: dhj 17613071153@163.com
+ * @LastEditTime: 2023-04-02 19:22:04
+ * @FilePath: \实验项目\.vitepress\config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "My Awesome Project",
+  description: "A VitePress Site",
+  base:'/vitepress/',
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
