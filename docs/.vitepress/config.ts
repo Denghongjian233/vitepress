5c5cf1ee3199f5b7eb7fdd5c810797dd232ed64d
dhj <17613071153@163.com>
2023-04-25 18:01:22 +0800

    feat:4.25

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,8 +2,8 @@
  * @Author: dhj 17613071153@163.com
  * @Date: 2023-04-02 19:19:37
  * @LastEditors: dhj 17613071153@163.com
- * @LastEditTime: 2023-04-24 17:59:59
- * @FilePath: \最近的项目\实验项目\docs\.vitepress\config.ts
+ * @LastEditTime: 2023-04-25 09:39:50
+ * @FilePath: \实验项目\docs\.vitepress\config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { defineConfig } from 'vitepress'
